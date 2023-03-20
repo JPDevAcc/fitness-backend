204e6ebd20f951931db83aaaddb769d3d1e7514e
JP <71121804+JPDevAcc@users.noreply.github.com>
2023-03-20 13:09:20 +0000

    Added change-pwd and delete-account endpoints

--- router.mjs
+++ router.mjs
@@ -1,4 +1,5 @@
 import express from "express";
+import * as userController from "./controllers/userController.mjs"
 import * as userPrefsController from "./controllers/userPrefsController.mjs" ;
 import * as userProfileController from "./controllers/userProfileController.mjs" ;
 import * as testController from "./controllers/testController.mjs" ;
@@ -6,6 +7,8 @@ import { getUnsplashPic } from "./controllers/unsplashController.mjs";
 const router = express.Router();
 
 // Main routes
+router.post("/changepass", userController.userChangePwd) ; // Change user-password
+router.post("/delaccount", userController.userDeleteAccount) ; // Delete user-account
 router.get("/prefs", userPrefsController.retrieve) ; // Retrieve user-prefs
 router.patch("/prefs/:fieldName", userPrefsController.updatePrefs) ; // Create or update user-prefs
 router.get("/profile", userProfileController.retrieve) ; // Retrieve user-profile
